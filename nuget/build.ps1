73a34d75d5c3b45b2305e8320136a735f252795e
RuxSmile <130905407+BoomSumer@users.noreply.github.com>
2023-04-26 00:20:44 +0800

    Create build.ps1

---
+++ nuget/build.ps1
@@ -0,0 +1,17 @@
+param(
+    [string]$version
+)
+
+# 定义变量
+$projectName = "SmileControls"
+$projectPath = "$PSScriptRoot\$projectName\$projectName.csproj"
+$nupkgOutputDirectory = "$PSScriptRoot\NuGetPackages"
+
+# 清理和构建项目
+msbuild $projectPath /p:Configuration=Release /t:Clean,Build
+
+# 创建NuGet包
+nuget pack $projectPath -Prop Configuration=Release -Version $version -OutputDirectory $nupkgOutputDirectory -Symbols
+
+# 将NuGet包推送到NuGet.org
+nuget push "$nupkgOutputDirectory\$projectName.$version.nupkg" -Source nuget.org -ApiKey $env:NuGetApiKey
